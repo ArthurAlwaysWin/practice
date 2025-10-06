375aee78aec874525404d3557141f63ea4727e17
LI XIAO <a3133221862@outlook.com>
2025-10-07 08:38:13 +1100

    first time doing this

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
 print('hello, how r u')
+
+class Student:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+    
+    def getinformation(self):
+        return f"my name is {self.name} and my age is {self.age}"
+    
+
+s1 = Student("Mike", 20)
+print(s1.getinformation())
